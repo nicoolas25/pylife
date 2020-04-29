2d678098679ff8d159f3fefa7d504037f8b54b6f
Nicolas Zermati <nicoolas25@gmail.com>
2020-04-29 09:49:54 +0200

    End of session

--- life.py
+++ life.py
@@ -1,2 +1,48 @@
-def func(_):
-    return 5
+# Size of the grid?
+#   => Limit to the given input
+# Initial state
+#   => From a multiline string
+# 1st step Reproduce the rules
+
+class Grid():
+    @staticmethod
+    def parse(input):
+        alive_positions = set()
+        for x, line in enumerate(input.split("\n")):
+            for y, cell in enumerate(line):
+                if cell == "#":
+                    alive_positions.add((x, y))
+        return Grid(alive_positions, x + 1)
+
+    def __init__(self, alive_positions, size):
+        self._alive_positions = alive_positions
+        self._size = size
+
+    def is_alive(self, x, y):
+        return (x, y) in self._alive_positions
+
+    def next_state(self):
+        alive_positions = set()
+        for x in range(self._size)):
+            for y in range(self._size):
+                if self._next_cell_liveness(x, y):
+                    alive_positions.add((x, y))
+        return Grid(alive_positions, self._size)
+
+    def _next_cell_liveness(self, x, y):
+        if self.is_alive(x, y) and self._alive_neighbors(x, y) in (2, 3):
+            return True # Survival
+        if not self.is_alive(x, y) and self._alive_neighbors(x, y) == 3:
+            return True # Reproduction
+
+        return False # underpop & overcrowding
+
+    def _alive_neighbors(self, x, y):
+        neighbors_positions = [(x-1, y-1), (x-1, y),  (x-1, y+1),
+                               (x,   y-1),            (x,   y+1),
+                               (x+1, y-1), (x+1, y),  (x+1, y+1)]
+        return sum([1 for x, y in neighbors_positions if self.is_alive(x, y)])
+
+    def __str__(self):
+        return "\n".join(self._input)
+
--- test_life.py
+++ test_life.py
@@ -3,5 +3,94 @@ import unittest
 
 class TestLife(unittest.TestCase):
 
-    def test_upper(self):
-        self.assertEqual(life.func(3), 5)
+    def test_grid_initialization(self):
+        input = self.grid("...",
+                          ".#.",
+                          "..#")
+        assert life.Grid(input)
+
+    def test_access_position_liveness(self):
+        input = self.grid("...",
+                          "..#",
+                          "...")
+        grid = life.Grid(input)
+        self.assertTrue(grid.is_alive(1, 2))
+        self.assertFalse(grid.is_alive(0, 0))
+
+    def test_next_cell_state_when_underpopulation(self):
+        input = self.grid("...",
+                          ".#.",
+                          "..#")
+        grid = life.Grid(input)
+        self.assertFalse(grid._next_cell_liveness(1, 1))
+
+    def test_next_cell_state_when_survival(self):
+        input = self.grid("...",
+                          "##.",
+                          ".##")
+        grid = life.Grid(input)
+        self.assertTrue(grid._next_cell_liveness(1, 1))
+
+    def test_next_cell_state_when_overcrowding(self):
+        input = self.grid("###",
+                          "###",
+                          "###")
+        grid = life.Grid(input)
+        self.assertFalse(grid._next_cell_liveness(1, 1))
+
+    def test_next_cell_state_when_reproduction(self):
+        input = self.grid("...",
+                          "#..",
+                          ".##")
+        grid = life.Grid(input)
+        self.assertTrue(grid._next_cell_liveness(1, 1))
+
+    def test_next_cell_state_when_underpopulation_when_dead(self):
+        input = self.grid("...",
+                          "#..",
+                          ".#.")
+        grid = life.Grid(input)
+        self.assertFalse(grid._next_cell_liveness(1, 1))
+
+    def test_underpopulation(self):
+        input = self.grid("...",
+                          ".#.",
+                          "..#")
+        grid = life.Grid(input)
+        expected_grid = self.grid("...",
+                                  "...",
+                                  "...")
+        self.assertEquals(str(grid.next_state()), expected_grid)
+
+    def test_survival(self):
+        input = self.grid("...",
+                          "##.",
+                          ".##")
+        grid = life.Grid(input)
+        expected_grid = self.grid("...",
+                                  "###",
+                                  "###")
+        self.assertEquals(str(grid.next_state()), expected_grid)
+
+    def test_overcrowding(self):
+        input = self.grid("###",
+                          "###",
+                          "###")
+        grid = life.Grid(input)
+        expected_grid = self.grid("#.#",
+                                  "...",
+                                  "#.#")
+        self.assertEquals(str(grid.next_state()), expected_grid)
+
+    def test_reproduction(self):
+        input = self.grid("...",
+                          "#..",
+                          ".##")
+        grid = life.Grid(input)
+        expected_grid = self.grid("...",
+                                  ".#.",
+                                  ".#.")
+        self.assertEquals(str(grid.next_state()), expected_grid)
+
+    def grid(self, *args):
+        return "\n".join([f"{l}" for l in args])
